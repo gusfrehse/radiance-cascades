7b89cc84986d84b8aca6e7f5efcb7756fe05b10c
Gustavo S Frehse <gusfrehse@gmail.com>
2024-11-10 21:09:22 -0300

    fix some cmake stuff, add base sdl3 code

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,31 @@
 
 int main(int argc, char **argv) {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
-        std::cout << "Failed to initialized SDL." << std::endl;
+        std::cout << "Failed to initialize SDL." << std::endl;
+    }
+
+    std::cout << "SDL initialized." << std::endl;
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+
+    SDL_Window *window = SDL_CreateWindow("Radiance Cascades", 1280, 720, SDL_WINDOW_OPENGL);
+
+    if (window == nullptr) {
+        std::cout << "Couldn't create window." << std::endl;
+        std::exit(1);
+    }
+
+    bool shouldQuit = false;
+    while (!shouldQuit) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                shouldQuit = true;
+            }
+        }
+
     }
 
     return 0;
