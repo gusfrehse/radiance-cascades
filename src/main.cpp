ead9968bb5a153eabc18d5248563e5a9b8cef1cf
Gustavo S Frehse <gusfrehse@gmail.com>
2024-11-11 22:27:58 -0300

    creating framebuffers and textures

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,62 @@
 #include "SDL3/SDL_error.h"
 #include "SDL3/SDL_video.h"
+#include <glbinding/gl/bitfield.h>
+#include <glbinding/gl/enum.h>
+#include <glbinding/gl/functions.h>
+#include <glbinding-aux/debug.h>
 #include <iostream>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <glbinding/gl/gl.h>
 #include <glbinding/glbinding.h>
 
+const int width = 1280;
+const int height = 720;
+
 int main(int argc, char **argv) {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
-        std::cout << "Failed to initialize SDL." << std::endl;
+        std::cerr << "Failed to initialize SDL." << std::endl;
     }
 
-    std::cout << "SDL initialized." << std::endl;
+    std::cerr << "SDL initialized." << std::endl;
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 
-    SDL_Window *window = SDL_CreateWindow("Radiance Cascades", 1280, 720, SDL_WINDOW_OPENGL);
+    SDL_Window *window = SDL_CreateWindow("Radiance Cascades", width, height, SDL_WINDOW_OPENGL);
 
     if (!window) {
-        std::cout << "Couldn't create window: " << SDL_GetError() << std::endl;
+        std::cerr << "Couldn't create window: " << SDL_GetError() << std::endl;
         std::exit(1);
     }
+
+    std::cerr << "Window created." << std::endl;
 
     SDL_GLContext context = SDL_GL_CreateContext(window);
     if (!context) {
-        std::cout << "Couldn't create OpenGL context: " << SDL_GetError() << std::endl;
+        std::cerr << "Couldn't create OpenGL context: " << SDL_GetError() << std::endl;
         std::exit(1);
     }
 
+    std::cerr << "Context created." << std::endl;
+
     glbinding::initialize(SDL_GL_GetProcAddress);
+    glbinding::aux::enableGetErrorCallback();
+
+    gl::GLuint framebuffers[4];
+    gl::glGenFramebuffers(4, framebuffers);
+
+    gl::GLuint textures[4];
+    gl::glCreateTextures(gl::GL_TEXTURE_2D, 4, textures);
+
+    for (int i = 0; i < 4; i++) {
+        gl::glTextureStorage2D(textures[i], 1, gl::GLenum::GL_RGBA8, width, height);
+        gl::glNamedFramebufferTexture(framebuffers[i], gl::GL_COLOR_ATTACHMENT0, textures[i], 0);
+    }
+
+
+    gl::glClearColor(0.4f, 0.5f, 0.2f, 1.0f);
 
     bool shouldQuit = false;
     while (!shouldQuit) {
@@ -40,6 +66,10 @@ int main(int argc, char **argv) {
                 shouldQuit = true;
             }
         }
+
+        gl::glClear(gl::GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
     }
 
     return 0;
